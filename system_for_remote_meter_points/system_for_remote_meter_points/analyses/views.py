570e6c7bc4bb0ede90ddd158bbb846500fc8443b
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-12-01 00:35:43 +0200

    fix unreadable code for analyses views

--- system_for_remote_meter_points/system_for_remote_meter_points/analyses/views.py
+++ system_for_remote_meter_points/system_for_remote_meter_points/analyses/views.py
@@ -7,16 +7,27 @@ from system_for_remote_meter_points.tasks.models import Task
 # Create your views here.
 def type_task_by_count(request):
 
-    restore_task_type = Task.objects.filter(operation="Restore communication").count()
-    add_new_meter_point_task_type = Task.objects.filter(operation="Add new meter points").count()
-    replace_meter_device_task_type = Task.objects.filter(operation="Replace meter device").count()
-    replace_modem_sim_task_type = Task.objects.filter(operation="Replace modem and/or SIM card").count()
-    delete_meter_point_task_type = Task.objects.filter(operation="Delete meter points").count()
-    add_new_constant_task_type = Task.objects.filter(operation="Add new constant").count()
-    in_comment_task_type = Task.objects.filter(operation="Other").count()
+    # restore_task_type = Task.objects.filter(operation="Restore communication").count()
+    # add_new_meter_point_task_type = Task.objects.filter(operation="Add new meter points").count()
+    # replace_meter_device_task_type = Task.objects.filter(operation="Replace meter device").count()
+    # replace_modem_sim_task_type = Task.objects.filter(operation="Replace modem and/or SIM card").count()
+    # delete_meter_point_task_type = Task.objects.filter(operation="Delete meter points").count()
+    # add_new_constant_task_type = Task.objects.filter(operation="Add new constant").count()
+    # in_comment_task_type = Task.objects.filter(operation="Other").count()
 
-    label = ['Restore communication', 'Add new meter points', 'Replace meter device', 'Replace modem and/or SIM card', 'Delete meter points', 'Add new constant', 'Other']
-    data = [restore_task_type, add_new_meter_point_task_type, replace_meter_device_task_type, replace_modem_sim_task_type, delete_meter_point_task_type, add_new_constant_task_type, in_comment_task_type]
+    # label = ['Restore communication', 'Add new meter points', 'Replace meter device', 'Replace modem and/or SIM card', 'Delete meter points', 'Add new constant', 'Other']
+    # data = [restore_task_type, add_new_meter_point_task_type, replace_meter_device_task_type, replace_modem_sim_task_type, delete_meter_point_task_type, add_new_constant_task_type, in_comment_task_type]
+
+    label = []
+    data = []
+
+    tasks = Task.objects.all()
+    for task in tasks:
+        if task.operation not in label:
+            label.append(task.operation)
+
+    for lab in label:
+        data.append(Task.objects.filter(operation=lab).count())
 
     context = {
         'label': label,
@@ -27,12 +38,16 @@ def type_task_by_count(request):
 
 def result_task_by_count(request):
 
-    successful_task = Task.objects.filter(result_operation="Successful communication").count()
-    failed_task = Task.objects.filter(result_operation="No communication").count()
-    in_progress_task = Task.objects.filter(result_operation="In progress...").count()
+    label = []
+    data = []
 
-    label = ['Successful communication', 'No communication', 'In progress...']
-    data = [successful_task, failed_task, in_progress_task]
+    tasks = Task.objects.all()
+    for task in tasks:
+        if task.result_operation not in label:
+            label.append(task.result_operation)
+
+    for lab in label:
+        data.append(Task.objects.filter(result_operation=lab).count())
 
     context = {
         'label': label,
@@ -44,14 +59,16 @@ def result_task_by_count(request):
 
 def count_type_meter_device(request):
 
-    meter_device_iskra = MeterDevice.objects.filter(meter_device_type='ISKRA').count()
-    meter_device_amt = MeterDevice.objects.filter(meter_device_type='AMT').count()
-    meter_device_microstar = MeterDevice.objects.filter(meter_device_type='Microstar').count()
-    meter_device_gama = MeterDevice.objects.filter(meter_device_type='Gama').count()
+    label = []
+    data = []
 
+    meter_devices = MeterDevice.objects.all()
+    for meter_device in meter_devices:
+        if meter_device.meter_device_type not in label:
+            label.append(meter_device.meter_device_type)
 
-    label = ['Iskra', 'AMT', 'Microstar', 'Game']
-    data = [meter_device_iskra, meter_device_amt, meter_device_microstar, meter_device_gama]
+    for lab in label:
+        data.append(MeterDevice.objects.filter(meter_device_type=lab).count())
 
     context = {
         'label': label,
