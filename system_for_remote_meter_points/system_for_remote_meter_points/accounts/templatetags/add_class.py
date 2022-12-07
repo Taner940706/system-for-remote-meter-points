1ed3e2d0c4cc8d130141dc83e0e351a485d05889
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-12-07 17:13:55 +0200

    add some new .css files, fix some navigation sidebar problems, starting to design templates

---
+++ system_for_remote_meter_points/system_for_remote_meter_points/accounts/templatetags/add_class.py
@@ -0,0 +1,21 @@
+from django.template import Library
+
+register = Library()
+
+
+@register.filter(name='add_classes')
+def add_classes(value, arg):
+
+    css_classes = value.field.widget.attrs.get('class', '')
+    # check if class is set or empty and split its content to list (or init list)
+    if css_classes:
+        css_classes = css_classes.split(' ')
+    else:
+        css_classes = []
+    # prepare new classes to list
+    args = arg.split(' ')
+    for a in args:
+        if a not in css_classes:
+            css_classes.append(a)
+    # join back to single string
+    return value.as_widget(attrs={'class': ' '.join(css_classes)})
